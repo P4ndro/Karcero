89af7ab7707a5e6c98ac3447b860add0a82a2c63
P4ndro <sandro02050502@gmail.com>
2025-12-05 03:32:22 +0400

    fix: Add error handling for missing DB_URL in MongoDB connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ import { ENV } from "./env.js";
 
 
 export const connectDB = async () => {
+    if(!ENV.DB_URL){
+        throw new Error("DB_URL is not set");
+    }
     try {
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log(`✅ MongoDB connected: ${conn.connection.host}`);
@@ -12,5 +15,3 @@ export const connectDB = async () => {
         process.exit(1); // 0 is success, 1 is failure
     }
 }
-
-export default { connectDB };
